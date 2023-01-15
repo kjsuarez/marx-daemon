918ce75651ab663435c02391e8debf4baf5fe191
Kevin Suarez <kjsuarez94@gmail.com>
2023-01-15 15:52:03 -0800

    light weight bot deployment via cron

--- bot_monitor.sh
+++ bot_monitor.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo "bundle: `which bundle`" > monitor.log
-if ruby ~/Code/marx-daemon/daemon_controller.rb status 
+if pgrep -f marx_daemon.rb
 then
-	echo "daemon already running `date`" > monitor.log
-	echo I see him running
+        echo "daemon already running `date`" > monitor.log
+        echo I see him running
 else
-	echo "starting daemon at `date` from `pwd` as `whoami`" > monitor.log
-	echo "I don't see him running"
-	bundle exec ruby ~/Code/marx-daemon/daemon_controller.rb start
-fi	
+        echo "starting daemon at `date` from `pwd` as `whoami`" > monitor.log
+        echo "I don't see him running"
+        exec ruby marx_daemon.rb >> marx_daemon.log 2>&1 &
+fi
--- daemon_controller.rb
+++
@@ -1,7 +0,0 @@
-#$stdout.reopen("daemon_controller.log","w")
-#$stderr.reopen("daemon_controller_err.log","w")
-puts "standard out goes here at #{Time.now}"
-require 'daemons'
-puts "got past daemon requirement"
-
-Daemons.run('~/Code/marx-daemon/marx_daemon.rb')
