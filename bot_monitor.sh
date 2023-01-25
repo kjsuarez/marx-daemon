29e6544abe2bb48427ab76f7121280616a4c5613
Kevin Suarez <kjsuarez94@gmail.com>
2023-01-24 18:06:09 -0800

    improved bot_monitor

--- bot_monitor.sh
+++ bot_monitor.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if pgrep -f marx_daemon.rb
-then
+if pgrep -f marx_daemon.rb; then
+	PROID=$(pgrep -f marx_daemon.rb)
         echo "daemon already running `date`" > monitor.log
         echo I see him running
+	DAEMON_STATE="$(ps -q $PROID -o state --no-headers)"
+	if [[ $DAEMON_STATE =~ "S" ]]; then 
+		kill $PROID
+		exec ruby marx_daemon.rb >> marx_daemon.log 2>&1 &
+	else 
+		echo not sleeping; 
+	fi
 else
         echo "starting daemon at `date` from `pwd` as `whoami`" > monitor.log
         echo "I don't see him running"
