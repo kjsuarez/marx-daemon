cc2520b4e2ccfb484f8a1a3443c9081509f862f0
Kevin Suarez <kjsuarez94@gmail.com>
2024-12-16 02:32:58 +0000

    change the dir that cron runs ruby from

--- bot_monitor.sh
+++ bot_monitor.sh
@@ -12,7 +12,7 @@ if pgrep -f marx_daemon.rb; then
 		echo not sleeping; 
 	fi
 else
+	echo "I don't see him running"
         echo "starting daemon at `date` from `pwd` as `whoami`" > monitor.log
-        echo "I don't see him running"
-        exec ruby marx_daemon.rb >> marx_daemon.log 2>&1 &
+        exec ruby -C /home/orangepi/code/ruby/marx-daemon marx_daemon.rb >> marx_daemon.log 2>&1 &
 fi
